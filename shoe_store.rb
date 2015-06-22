e8375fe36ce172b6ee35f3652730fa197c3f231f
Mathieu Gagné <mgagne@vigilantglobal.com>
2015-06-22 16:26:09 -0400

    Adds shoe models

--- shoe_store.rb
+++ shoe_store.rb
@@ -5,12 +5,16 @@ require 'json'
 STDOUT.sync = true
 
 STORE_IDS = Array(1..10)
+SHOES_IDS = Array(1425..1440)
 INVENTORY = Array(0..100)
 RANDOMNESS = Array(1..3)
 
 loop do
   RANDOMNESS.sample.times do
-    puts JSON.generate({stream: "shoe-store-#{STORE_IDS.sample}", inventory: INVENTORY.sample}, quirks_mode: true)
+    puts JSON.generate({
+      stream: "shoe-store-#{STORE_IDS.sample}",
+      "shoe-#{SHOES_IDS.sample}" => INVENTORY.sample,
+    }, quirks_mode: true)
   end
   sleep 1
 end
