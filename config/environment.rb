927ff429a0a4aba359f54e1a3d5ac9dbec4f94a6
Clement Morisset <morissetcl87@gmail.com>
2020-04-28 22:46:08 +0200

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/shoe_store'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/shoe_store_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/shoe_store_development'
+    #    adapter :sql, 'mysql://localhost/shoe_store_development'
+    #
+    adapter :sql, ENV.fetch('DATABASE_URL')
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/shoe_store/mailers'
+
+    # See https://guides.hanamirb.org/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: https://guides.hanamirb.org/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json, filter: []
+
+    mailer do
+      delivery :smtp, address: ENV.fetch('SMTP_HOST'), port: ENV.fetch('SMTP_PORT')
+    end
+  end
+end
---
+++ lib/shoe_store.rb
@@ -0,0 +1,2 @@
+module ShoeStore
+end
