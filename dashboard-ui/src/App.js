08a906796750deb69970c9f68cac733658c9c1e9
Carlos Figueiredo <carlos.figueiredo87@gmail.com>
2020-08-08 03:40:12 -0400

    Create container to show inventory from sample alert
    The container renders list of items and their related inventory
    separated for store, but I'm testing for a 1 static sample alert without
    possible duplication if "receiving" the alert for the same model/store
    and different inventory quantity
    
    {
      'store': 'ALDO Ste-Catherine',
      'model': 'ADERI',
      'inventory': 100
    }

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+const sampleAlert = {
+  'store': 'ALDO Ste-Catherine',
+  'model': 'ADERI',
+  'inventory': 100
+};
+
 function App() {
+  let inventory = [ sampleAlert ];
+  const stores = ['ALDO Ste-Catherine'];
+
   return (
-    <div className="App">
-    </div>
+    <>
+    { stores.map((store) => {
+      const storeItems = inventory.filter((inventoryItem) => { return (inventoryItem.store === store)});
+
+      return (
+        <div id={store}>
+          <header>{store}</header>
+          <ul>
+            { storeItems.map((item) => {
+                return (
+                  <li id={item.model }>{item.model} - {item.inventory}</li>
+                );
+            })}
+          </ul>
+        </div>
+        );
+    }) }
+    </>
   );
 }
 
